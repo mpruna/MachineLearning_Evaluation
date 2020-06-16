9018b1d4d916608270543cd9b23d5a82e51c3dd7
praslea <pruna_mihai@yahoo.com>
2020-06-16 23:20:58 +0300

    py script tweek

--- eval_error.py
+++ eval_error.py
@@ -1,10 +1,14 @@
+#!/home/anaconda3/envs/py37_covidenv/bin/python
 import numpy as np
 
 
 #Mean Absolute Error
 def mae(predict, actual):
+
+    '''
     predict = np.array(predict)
     actual = np.array(actual)
+    '''
 
     difference = abs(predict - actual)
     score = difference.mean()
@@ -13,8 +17,10 @@ def mae(predict, actual):
 
 #Mean Squared Error
 def mse(predict, actual):
+    '''
     predict = np.array(predict)
     actual = np.array(actual)
+    '''
 
     difference = predict - actual
     square_diff = np.square(difference)
@@ -24,8 +30,10 @@ def mse(predict, actual):
 
 #Root Mean Squared Error
 def rmse(predict, actual):
+    '''
     predict = np.array(predict)
     actual = np.array(actual)
+    '''
 
     difference = predict - actual
     square_diff = np.square(difference)
@@ -35,8 +43,10 @@ def rmse(predict, actual):
 
 #Root Mean Square Logarithmic Error
 def rmsle(predict, actual):
+    '''
     predict = np.array(predict)
     actual = np.array(actual)
+    '''
 
     log_predict = np.log(predict+1)
     log_actual = np.log(actual+1)
@@ -51,9 +61,10 @@ def rmsle(predict, actual):
 
 #Mean Bias Deviation
 def mbd(predict, actual):
+    '''
     predict = np.array(predict)
     actual = np.array(actual)
-
+    '''
     difference = predict - actual
     numerator = np.sum(difference) / len(predict) 
     denumerator =  np.sum(actual) / len(predict)
@@ -63,3 +74,24 @@ def mbd(predict, actual):
     score = float(numerator) / denumerator * 100
 
     return score
+
+x = np.linspace(0, 100, 50)
+y = 2*x + 1
+
+noise = np.random.normal(0, 7, y.shape)
+y_2 = y + noise
+
+mae=mae(predict=y, actual=y_2)
+print("MAE error {}".format(mae))
+
+mse=mse(predict=y, actual=y_2)
+print("MSE error {}".format(mse))
+
+rmse=rmse(predict=y, actual=y_2)
+print("RMSE error {}".format(rmse))
+
+rmsle=rmsle(predict=y, actual=y_2)
+print("RMSLE error {}".format(rmsle))
+
+mbd=mbd(predict=y, actual=y_2)
+print("MBD error {}".format(mbd))
