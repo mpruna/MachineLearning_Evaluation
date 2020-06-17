3951f7fee6e89e9208d05d77720d938cfe273714
praslea <pruna_mihai@yahoo.com>
2020-06-17 08:04:34 +0300

    rm comment

--- eval_error.py
+++ eval_error.py
@@ -5,22 +5,14 @@ import numpy as np
 #Mean Absolute Error
 def mae(predict, actual):
 
-    '''
-    predict = np.array(predict)
-    actual = np.array(actual)
-    '''
-
     difference = abs(predict - actual)
     score = difference.mean()
 
     return score
 
 #Mean Squared Error
-def mse(predict, actual):
-    '''
-    predict = np.array(predict)
-    actual = np.array(actual)
-    '''
+def mse(predict, actual):actual)
+
 
     difference = predict - actual
     square_diff = np.square(difference)
@@ -30,10 +22,6 @@ def mse(predict, actual):
 
 #Root Mean Squared Error
 def rmse(predict, actual):
-    '''
-    predict = np.array(predict)
-    actual = np.array(actual)
-    '''
 
     difference = predict - actual
     square_diff = np.square(difference)
@@ -43,10 +31,6 @@ def rmse(predict, actual):
 
 #Root Mean Square Logarithmic Error
 def rmsle(predict, actual):
-    '''
-    predict = np.array(predict)
-    actual = np.array(actual)
-    '''
 
     log_predict = np.log(predict+1)
     log_actual = np.log(actual+1)
@@ -61,10 +45,7 @@ def rmsle(predict, actual):
 
 #Mean Bias Deviation
 def mbd(predict, actual):
-    '''
-    predict = np.array(predict)
-    actual = np.array(actual)
-    '''
+
     difference = predict - actual
     numerator = np.sum(difference) / len(predict) 
     denumerator =  np.sum(actual) / len(predict)
